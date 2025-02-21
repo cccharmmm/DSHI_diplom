de440b45e9849e9b6c7487fcb68c52032d839d91
Dilyara <dilyara.sadykova1414@mail.ru>
2025-02-21 21:48:13 +0300

    Корректная работа с ЛК

--- DSHI_diplom/Components/Pages/Profile.razor.cs
+++ DSHI_diplom/Components/Pages/Profile.razor.cs
@@ -5,6 +5,7 @@ using Blazored.LocalStorage;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace DSHI_diplom.Components.Pages
 {
@@ -16,6 +17,12 @@ namespace DSHI_diplom.Components.Pages
 
         private User _userProfile = new User();
         private string token;
+        private string? successMessage;
+        private string messageColor = "forestgreen";
+        private bool showConfirmationModal = false;
+        private bool _isFormValid = false;
+        private bool isFormSubmitting = false;
+        private bool isFormHandled = false;
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
@@ -55,7 +62,93 @@ namespace DSHI_diplom.Components.Pages
                 Console.WriteLine("Не удалось получить данные пользователя.");
             }
         }
+        
+         private void ConfirmSaveChanges()
+        {
+            showConfirmationModal = true;
+            _isFormValid = true; 
+            Console.WriteLine("Модальное окно открыто");
+        }
 
+        private void CancelSave()
+        {
+            showConfirmationModal = false;
+            _isFormValid = false;
+            isFormSubmitting = false;
+            isFormHandled = false;
+            Console.WriteLine("CancelSave вызван:");
+            InvokeAsync(() => StateHasChanged());
+            ResetForm();
+            Console.WriteLine("Модальное окно закрыто");
+        }
 
+        private async Task SaveChanges()
+        {
+            showConfirmationModal = false;
+            _isFormValid = false;
+            isFormSubmitting = false;
+            isFormHandled = false; 
+
+            await InvokeAsync(StateHasChanged);
+
+            Console.WriteLine($"showConfirmationModal: {showConfirmationModal}, _isFormValid: {_isFormValid}");
+
+            var token = await LocalStorage.GetItemAsync<string>("authToken");
+            if (string.IsNullOrEmpty(token))
+            {
+                successMessage = "Ошибка: токен не найден";
+                messageColor = "red";
+                await HideErrorAfterDelay();
+                return;
+            }
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Put, $"api/user/{_userProfile.Id}");
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            requestMessage.Content = new StringContent(JsonConvert.SerializeObject(_userProfile));
+            requestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await Http.SendAsync(requestMessage);
+            if (response.IsSuccessStatusCode)
+            {
+                successMessage = "Данные успешно сохранены";
+                messageColor = "forestgreen";
+                await InvokeAsync(StateHasChanged);
+                await HideErrorAfterDelay();
+                Console.WriteLine("Данные изменены");
+            }
+            else
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                successMessage = "Ошибка сохранения данных";
+                messageColor = "red";
+                await HideErrorAfterDelay();
+                await InvokeAsync(StateHasChanged);
+                Console.WriteLine($"Ошибка сохранения: {errorContent}");
+            }
+
+            ResetForm();
+        }
+        private async Task HideErrorAfterDelay()
+        {
+            await Task.Delay(3000);
+            successMessage = null;
+            await InvokeAsync(StateHasChanged);
+        }
+     
+        private void HandleInvalidSubmit()
+        {
+            _isFormValid = false;
+            showConfirmationModal = false;
+            isFormSubmitting = false;
+            isFormHandled = false;
+        }
+        private void ResetForm()
+        {
+            _isFormValid = false;
+            showConfirmationModal = false;
+            isFormSubmitting = false;
+             isFormHandled = false;
+
+        }
     }
 }
--- DSHI_diplom/Model/User.cs
+++ DSHI_diplom/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DSHI_diplom.Model;
 
@@ -7,14 +8,29 @@ public partial class User
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Поле фамилии обязательно для заполнения")]
+    [StringLength(15, ErrorMessage = "Фамилия должна содержать не более 15 символов")]
+    [RegularExpression(@"^[А-ЯЁ][а-яё]*$", ErrorMessage = "Проверьте корректность поля")]
     public string LastName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Поле имени обязательно для заполнения")]
+    [StringLength(15, ErrorMessage = "Имя должно содержать не более 15 символов")]
+    [RegularExpression(@"^[А-ЯЁ][а-яё]*$", ErrorMessage = "Проверьте корректность поля")]
     public string Name { get; set; } = null!;
 
+    [Required(ErrorMessage = "Поле отчества обязательно для заполнения")]
+    [StringLength(15, ErrorMessage = "Отчество должно содержать не более 15 символов")]
+    [RegularExpression(@"^[А-ЯЁ][а-яё]*$", ErrorMessage = "Проверьте корректность поля")]
     public string MiddleName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Поле логина обязательно для заполнения")]
+    [StringLength(10, ErrorMessage = "Логин должен содержать не более 10 символов")]
+    [RegularExpression(@"^[a-z0-9_.]+$", ErrorMessage = "Логин должен содержать только латинские буквы, цифры, символы _ и .")]
     public string Login { get; set; } = null!;
 
+    [Required(ErrorMessage = "Поле пароля обязательно для заполнения")]
+    [StringLength(15, MinimumLength = 6, ErrorMessage = "Пароль должен содержать не менее 6 и не более 15 символов")]
+    [RegularExpression(@"^[^\p{IsCyrillic}]*$", ErrorMessage = "Проверьте корректность поля")]
     public string Password { get; set; } = null!;
 
     public int? RoleId { get; set; }
